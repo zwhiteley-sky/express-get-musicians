da428ed1e038422c3b3bb20e8e3ff6d62dd10d90
Zachary Whiteley <zachary.whiteley@sky.uk>
2023-11-07 12:48:54 +0000

    Complete day two workshop

--- index.test.js
+++ index.test.js
@@ -24,4 +24,34 @@ describe('./musicians endpoint', () => {
 
         expect(response.body).toMatchObject(seedMusician.seedMusician[0]);
     });
+
+    test("can create a musician", async () => {
+        const response = await request(app)
+            .post("/musicians")
+            .send({ name: "Zachary", instrument: "Triangle" });
+
+        expect(response.body).toMatchObject({ name: "Zachary", instrument: "Triangle" });
+
+        const users = await request(app).get("/musicians");
+        expect(users.body.length).toBe(4);
+    });
+
+    test("can update a musician", async () => {
+        const response = await request(app)
+            .put("/musicians/4")
+            .send({ name: "Zach" });
+
+        expect(response.body).toMatchObject({ name: "Zach", instrument: "Triangle" });
+
+        const user = await request(app).get("/musicians/4");
+        expect(user.body).toMatchObject({ name: "Zach", instrument: "Triangle" });
+    });
+
+    test("can delete musician", async () => {
+        const response = await request(app)
+            .delete("/musicians/4")
+            .expect(204);
+
+        const user = await request(app).get("/musicians/4").expect(404);
+    });
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/musicians", async (req, res) => {
     res.json(await Musician.findAll());
 });
@@ -17,6 +19,41 @@ app.get("/musicians/:id", async (req, res) => {
 
     if (music) res.json(music);
     else res.status(404).send("musician not found");
+});
+
+app.post("/musicians", async (req, res) => {
+    const { name, instrument } = req.body;
+
+    const music = await Musician.create({ name, instrument });
+    res.json(music);
+});
+
+app.put("/musicians/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) return res.status(400).send("invalid id");
+
+    const { name, instrument } = req.body;
+
+    const original = await Musician.findByPk(id);
+
+    if (!original) return res.status(404).send("could not find musician");
+    
+    const updated = await original.update({ name, instrument });
+
+    res.json(updated);
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) return res.status(400).send("invalid id");
+
+    const original = await Musician.findByPk(id);
+
+    if (!original) return res.status(404).send("could not find musician");
+
+    await original.destroy();
+
+    res.status(204).send();
 });
 
 module.exports = app;
