99b5a5cb91c3466e17ffca2675bc25b044668f50
Zachary Whiteley <zachary.whiteley@sky.uk>
2023-11-08 12:25:32 +0000

    Add musician router

---
+++ src/music_router.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { Musician } = require("../models/index")
+const { db } = require("../db/connection")
+const music_router = express.Router();
+
+music_router.get("/", async (req, res) => {
+    res.json(await Musician.findAll());
+});
+
+music_router.get("/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) return res.status(400).send("invalid id");
+
+    const music = await Musician.findByPk(id);
+
+    if (music) res.json(music);
+    else res.status(404).send("musician not found");
+});
+
+music_router.post("/", async (req, res) => {
+    const { name, instrument } = req.body;
+
+    const music = await Musician.create({ name, instrument });
+    res.json(music);
+});
+
+music_router.put("/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) return res.status(400).send("invalid id");
+
+    const { name, instrument } = req.body;
+
+    const original = await Musician.findByPk(id);
+
+    if (!original) return res.status(404).send("could not find musician");
+    
+    const updated = await original.update({ name, instrument });
+
+    res.json(updated);
+});
+
+music_router.delete("/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) return res.status(400).send("invalid id");
+
+    const original = await Musician.findByPk(id);
+
+    if (!original) return res.status(404).send("could not find musician");
+
+    await original.destroy();
+
+    res.status(204).send();
+});
+
+module.exports = { music_router };
